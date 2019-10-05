67fc3b2711d7b9cc948c31f1045a1b5b50084f69
Leonardo Lindo <leojlindo@gmail.com>
2019-10-05 15:00:34 -0700

    fixed navigation and moved LoginForm to Login

--- food-friend/src/pages/login/Login.js
+++ food-friend/src/pages/login/Login.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, KeyboardAvoidingView } from 'react-native';
-import LoginForm from './LoginForm'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, KeyboardAvoidingView, StatusBar } from 'react-native';
 
 /* renders the main page for the login screen */
 export default class Login extends Component { 
@@ -17,8 +16,32 @@ export default class Login extends Component {
                             Welcome to Food Buddy!
                         </Text>
                     </View>
-                    <View style={StyleSheet.formContainer}>
-                        <LoginForm /> 
+                    <View style={styles.formContainer}>
+                        <StatusBar
+                        barStyle="light-content"
+                        />
+                        <TextInput 
+                            placeholder="username or email"
+                            placeholderTextColor="rgba(255,255,255,0.7)"
+                            returnKeyType="go"
+                            onSubmitEditing={() => this.passwordInput.focus()}
+                            keyboardType="email-address"
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            style={styles.input}  
+                            />
+                        <TextInput 
+                            placeholder="password"
+                            placeholderTextColor="rgba(255,255,255,0.7)"
+                            returnKeyType="go"
+                            secureTextEntry
+                            style={styles.input} 
+                            ref={(input) => this.passwordInput = input}
+                            />
+                        <TouchableOpacity style={styles.buttonContainer}
+                            onPress={() => this.props.navigation.navigate('Homepage')}>
+                                <Text style={styles.buttonText}>LOGIN</Text>
+                        </TouchableOpacity>
                     </View>
                 </View>
             </KeyboardAvoidingView>
@@ -43,12 +66,30 @@ const styles = StyleSheet.create({
         width: 100,
         height: 100
     },
-    
+    formContainer: {
+        padding: 20
+    },
     title: {
         color:'#FFFFFF',
         marginTop: 10,
         width: 14,
         textAlign: 'center'
+    },
+    input: {
+        height: 40,
+        backgroundColor: 'rgba(255,255,255,0.2)',
+        marginBottom: 10,
+        color:'#FFF',
+        paddingHorizontal: 10
+    },
+    buttonContainer: {
+        backgroundColor:'#2980b9',
+        paddingVertical:15
+    },
+    buttonText:{
+        textAlign: 'center',
+        color:'#FFFFFF',
+        fontWeight:'700'
     }
     
 });
