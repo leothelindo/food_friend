8e3c0faa1aacca97f99df645195240bdea48a3fe
Leonardo Lindo <leojlindo@gmail.com>
2019-10-14 23:08:47 -0700

    added permissions and map view with google api

--- food-friend/App.js
+++ food-friend/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, PermissionsAndroid } from 'react-native';
 
 import Login from './src/pages/login/Login';
 import Homepage from './src/pages/homepage/Homepage';
@@ -8,6 +8,8 @@ import RestaurantDetails from './src/pages/homepage/RestaurantDetails';
 
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation'
+
+
 
 const AppNavigator = createStackNavigator(
   {
@@ -33,6 +35,25 @@ const AppNavigator = createStackNavigator(
 const AppContainer = createAppContainer(AppNavigator);
 /* opens the login page when opening the app */
 export default class App extends Component{
+  /* for android devices asks for location permission */
+  async requestLocationPermission() {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          'title': 'Location Access Permission',
+          'message': 'This App needs access to your location ' +                    'so you can use it.'
+        }
+      )
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("You can use the location")
+      } else {
+        console.log("Location permission denied")
+      }
+    } catch (err) {
+      console.warn(err)
+    }
+  }
   render() {
     return (
       <AppContainer />
---
+++ food-friend/src/pages/map/Map.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { MapView } from 'expo';
+export default class Map extends React.Component {
+  constructor() {
+    super();
+this.state = {
+      origin: { latitude: 40.7050758, longitude: -74.0091604 },
+    };
+getLocation = () => {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          let newOrigin = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          };
+this.setState({
+            origin: newOrigin,
+          });
+          resolve(true);
+        },
+        err => {
+          console.log(err);
+          reject(reject);
+        },
+        { enableHighAccuracy: true, timeout: 2000, maximumAge: 1000 }
+      );
+    });
+  };
+async componentDidMount() {
+    await this.getLocation();
+  }
+render() {
+    return (
+      <MapView
+        style={{ flex: 1 }}
+        provider="google"
+        region={{
+          latitude: this.state.origin.latitude,
+          longitude: this.state.origin.longitude,
+          latitudeDelta: 0.0100,
+          longitudeDelta: 0.0100,
+        }}
+      >
+        <MapView.Marker coordinate={this.state.origin}/>
+      </MapView>
+    );
+  }
+}
