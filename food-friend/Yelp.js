09963fc9a1ac76c21d3b22848c6f1545a6c9f310
mmcool12 <mlanders@hmc.edu>
2019-10-29 15:31:33 -0700

    Added comments

--- food-friend/Yelp.js
+++ food-friend/Yelp.js
@@ -5,6 +5,8 @@ export const longLat = (location) => {
     return [long, lat]
 }
 
+//Searches the Yelp API for businesses near the user with the keyword delivery. 
+//Returns an array of 50 buisnesses
 export async function delSearch(long, lat) {
 
     var key = `iqmcCj-JKZ3r-lU3VOw-WkGWkvJeg52DvrMzf2tmxa83mOaBfs-58OI4If3PMF0oLUwuc9eRkGX-m3SpEoV-CizA1VGPo8BmnYVOPnsj7Fmvy_3YUw_NZ1vNbMOzXXYx`
